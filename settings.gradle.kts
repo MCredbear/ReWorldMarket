cfcb8f4150d0a4281fff5824032cf424adc466ff
Krysztal112233 <suibing112233@outlook.com>
2023-08-20 00:00:45 +0800

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+rootProject.name = "kotlin-paper-template"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://repo.papermc.io/repository/maven-public/")
+    }
+}
